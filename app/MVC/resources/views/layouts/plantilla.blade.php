4547877556ab93fad18830e3f6cf4c10f491cbbf
Miquel <mplanissi@gmail.com>
2024-03-08 22:59:05 +0100

    prova arreglar ajax

--- app/MVC/resources/views/layouts/plantilla.blade.php
+++ app/MVC/resources/views/layouts/plantilla.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="http://www.househostpromp.me/build/assets/custom.css">
-    <script src="http://www.househostpromp.me/build/assets/custom2.js"></script>
+    <link rel="stylesheet" type="text/css" href="www.househostpromp.me/build/assets/custom.css">
+    <script src="www.househostpromp.me/build/assets/custom2.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
     <!-- Map -->
