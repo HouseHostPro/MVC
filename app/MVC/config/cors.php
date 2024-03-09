a838beffa3d5809477e92780a24906ccf6fa281a
Miquel <mplanissi@gmail.com>
2024-03-09 18:40:46 +0100

    CORS

--- app/MVC/config/cors.php
+++ app/MVC/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['casconcos.live', '*.casconcos.live', 'househostpromp.me', '*.househostpromp.me'],
 
     'allowed_origins_patterns' => [],
 
